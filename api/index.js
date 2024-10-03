b6acc821f991b63413e28ce0034ab2a593d4e40a
Vivek Bhatt <the9416legend@gmail.com>
2024-10-03 06:45:20 +0530

    create signup api routes

--- api/index.js
+++ api/index.js
@@ -1,9 +1,26 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => {
+    console.log("Unable to connect error " + err);
+  });
 
 const app = express();
 
-const PORT = '3000';
+const PORT = "3000";
 
 app.listen(PORT, () => {
-    console.log(`Server Listening on port ${PORT} `);
+  console.log(`Server Listening on port ${PORT} `);
 });
+
+app.use(express.json());
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
